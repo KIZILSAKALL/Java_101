d5782e57a75087b90c656ba81593940b3a8288f0
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-05 13:38:50 +0300

    BodyMaxIndex finished

---
+++ Java_101/bodyMaxIndex.java
@@ -0,0 +1,20 @@
+package Java_101;
+import java.util.Scanner;
+public class bodyMaxIndex {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Please enter your heigt and weight");
+
+        System.out.print("Height(m): ");
+        double height=scan.nextDouble();
+
+        System.out.print("Weight(kg): ");
+        double weight=scan.nextDouble();
+
+        double bodyMaxIndex=weight/Math.pow(height, 2);
+        System.out.print("Your body max ındex: "+bodyMaxIndex);
+
+    }
+    
+}
