4a943be25fd7562d189ce7f807df59f4caf04f37
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-05 20:49:44 +0300

    userLogin has been finished

---
+++ Java_101/userLogin.java
@@ -0,0 +1,50 @@
+package Java_101;
+import java.util.*;
+public class userLogin {
+    public static void main(String args[]){
+        Scanner scan= new Scanner(System.in);
+
+        String username="BATUHAN";
+        String password="Mimisabu9999.";
+
+        System.out.print("Please enter your username: ");
+        String isusername= scan.nextLine();
+
+        System.out.print("Please enter your password: ");
+        String isPassword= scan.nextLine();
+
+        if(username.equals(isusername) && password.equals(isPassword)){
+            System.out.println("The system has been logged in");
+        }else{
+            System.out.println("Your informations are false.");
+        }
+
+        if(!(password.equals(isPassword))){
+            System.out.println("Your password is false. Do you want to change password ?");
+            System.out.println("1)Yes\n2)False");
+            int choice =scan.nextInt();
+
+            if(choice==1){
+                System.out.print("New password: ");
+                String newPassword=scan.next();
+
+                if(newPassword.equals(password)){
+                    System.out.println("The password couldn't be created. Please enter another password.");
+                }else{
+                    System.out.println("The password could be created.");
+                    password=newPassword;
+                    System.out.println(password);
+                }
+            }
+
+        }else{
+            System.out.println("Your username is wrong !");
+        }
+        
+
+        
+
+    
+    }
+    
+}
