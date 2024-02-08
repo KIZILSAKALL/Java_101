b74c4258ab600e1d1fd0fbaad3fca88f1629bff9
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-08 20:58:18 +0300

    the sentences changes

--- Java_101/leapYear.java
+++ Java_101/leapYear.java
@@ -10,7 +10,7 @@ public class leapYear {
         if(year%400==0 || year%4==0 && year%100 !=0){
             System.out.println(year+" is a leap year.");
         }else{
-            System.out.println(year+" is not a leap");
+            System.out.println(year+" is not a leap year");
         }
 
 
