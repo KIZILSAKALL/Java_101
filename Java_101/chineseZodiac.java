4f1c2090d702851ba9f95b2d740df21e03d38d45
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-08 20:33:33 +0300

    chinese zodiac has been finished

---
+++ Java_101/chineseZodiac.java
@@ -0,0 +1,58 @@
+package Java_101;
+import java.util.*;
+public class chineseZodiac {
+    public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+
+        System.out.print("Enter your year of birth: ");
+        int birth =scan.nextInt();
+
+        int division_12= birth%12;
+        
+        switch(division_12){
+            case 0 :
+            System.out.println("Your sign is Monkey");
+
+            case 1 :
+            System.out.println("Your sign is Cock");
+
+            case 2 :
+            System.out.println("Your sign is Dog");
+
+            case 3 :
+            System.out.println("Your sign is Pigs");
+
+            case 4 :
+            System.out.println("Your sign is Mouse");
+
+            case 5 :
+            System.out.println("Your sign is Ox");
+
+            case 6 :
+            System.out.println("Your sign is Tiger");
+
+            case 7 :
+            System.out.println("Your sign is Rabbit");
+
+            case 8 :
+            System.out.println("Your sign is Dragon");
+
+            case 9 :
+            System.out.println("Your sign is Snake");
+
+            case 10 :
+            System.out.println("Your sign is Horse");
+
+            case 11 :
+            System.out.println("Your sign is Sheep");
+
+            
+
+            
+
+        
+        }
+
+    }
+    
+}
