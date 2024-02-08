addfebf2c2dafe204cef25f35517f2a365dc795c
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-08 20:57:30 +0300

    leapYear has been finished

---
+++ Java_101/leapYear.java
@@ -0,0 +1,20 @@
+package Java_101;
+import java.util.*;
+public class leapYear {
+    public static void main(String[] args){
+        Scanner scan= new Scanner(System.in);
+
+        System.out.print("Please enter any year: ");
+        int year=scan.nextInt();
+
+        if(year%400==0 || year%4==0 && year%100 !=0){
+            System.out.println(year+" is a leap year.");
+        }else{
+            System.out.println(year+" is not a leap");
+        }
+
+
+    }
+
+    
+}
