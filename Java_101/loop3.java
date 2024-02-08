39a82adbc5b849f9877eedecd744ed897dde7d03
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-09 01:41:11 +0300

    loop3 has been finished

---
+++ Java_101/loop3.java
@@ -0,0 +1,28 @@
+package Java_101;
+import java.util.*;
+public class loop3 {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        
+        //TASK1//
+        System.out.print("Enter any number: ");
+        int number=scan.nextInt();
+
+        for (int i = 2; i <=number; i*=2) {
+        
+            System.out.println(i);
+        }
+
+
+        //TASK2//
+        System.out.print("Enter any number: ");
+        int number2=scan.nextInt();
+
+        for (int i = 1; i <=number2; i++) {
+            if(i%4==0 || i%5==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
