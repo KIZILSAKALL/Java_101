eda22bb239072efb7081a092d2d4e2124d3970e6
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-09 20:52:29 +0300

    harmonic Series has been finished

---
+++ Java_101/harmonicSeries.java
@@ -0,0 +1,20 @@
+package Java_101;
+import java.util.*;
+public class harmonicSeries {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Please enter any number and learn it's harmonic series: ");
+        int number=scan.nextInt();
+        double result=0;
+        for (int i = 1; i<=number ; i++) {
+            result+=(1.0/i);
+            
+        }
+
+        System.out.println(result);
+
+
+    }
+    
+}
