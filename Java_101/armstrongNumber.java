f869122024e4c3a4f12e82e748cea788bc8666d9
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-09 19:21:44 +0300

    armstrongNumber has been finished

---
+++ Java_101/armstrongNumber.java
@@ -0,0 +1,42 @@
+package Java_101;
+import java.util.*;
+public class armstrongNumber {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        int sum=0;
+        int numOfDig=0;
+        int remainder;
+        int sumOfRemainder=0;
+        int remainderValue=1;
+
+        System.out.print("Please enter any number and learn is it armstrong number: ");
+        int isArmstrongNumber=scan.nextInt();
+        int firstclone=isArmstrongNumber;
+
+        while(firstclone !=0){
+            firstclone /=10;
+            numOfDig++;
+        }
+
+        firstclone=isArmstrongNumber;
+
+        while(firstclone !=0){
+            remainder=firstclone%10;
+            for (int i = 0; i < numOfDig; i++) {
+                remainderValue*=remainder;
+            }
+            sumOfRemainder+=remainderValue;
+            remainderValue=1;
+            firstclone/=10;
+        }
+
+        if(sumOfRemainder==isArmstrongNumber){
+            System.out.println("Yes "+isArmstrongNumber+" is armstrong number.");
+        }else{
+            System.out.println("No "+isArmstrongNumber+" is not armstrong number.");
+
+        }
+    }
+    
+}
