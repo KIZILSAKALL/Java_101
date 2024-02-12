67bc8ad87642c015dcb68bd2cf6a8243e762d65d
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-12 18:23:59 +0300

    fibonacci has been finished

---
+++ Java_101/fibonacci.java
@@ -0,0 +1,28 @@
+package Java_101;
+import java.util.*;
+
+public class fibonacci {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("Please enter how many numbers you want from fibonacci Series: ");
+        int num=scan.nextInt();
+
+        int a=0 , b=1;
+    
+
+      
+        for (int i = 1; i <=num ; i++) {
+
+            System.out.print(a+" ");
+
+             int sum=a+b;
+             a=b;
+             b=sum;
+
+            
+        }
+
+    
+    }
+}
