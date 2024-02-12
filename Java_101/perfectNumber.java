6fc6a28728951c6796c6d181d4176cd8aa7a6113
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-12 15:59:16 +0300

    perfect number has been finished

---
+++ Java_101/perfectNumber.java
@@ -0,0 +1,27 @@
+package Java_101;
+import java.util.*;
+
+public class perfectNumber {
+    public static void main(String[] args){
+
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Enter a number and learn is it perfect number: ");
+        int isPerfect=scan.nextInt();
+
+        int sum=0;
+
+        for (int i =1; i < isPerfect; i++) {
+        
+            if(isPerfect%i==0){
+                sum+=i;
+            }
+        }
+
+        if(sum==isPerfect){
+            System.out.println(isPerfect+" is a perfect number.");
+        }else{
+            System.out.println(isPerfect+" is not a perfect number.");
+        }
+    }
+}
