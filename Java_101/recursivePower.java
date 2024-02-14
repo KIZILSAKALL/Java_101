8c212dda313f5df8eede8a231e0770abc87217dc
Batuhan Erdem <erdembatuhan0069@gmail.com>
2024-02-14 16:23:43 +0300

    recursivePower has been finished

---
+++ Java_101/recursivePower.java
@@ -0,0 +1,26 @@
+package Java_101;
+import java.util.*;
+public class recursivePower {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+
+        System.out.print("Please enter base number = ");
+        int base=scan.nextInt();
+
+        System.out.print("Please enter to the power of = ");
+        int power= scan.nextInt();
+
+        System.out.println("Result = "+recursivePower(base, power));
+
+    }
+
+    public static int recursivePower(int base, int power){
+
+      
+        if(power==0){
+            return 1;
+        }else{
+            return base*recursivePower(base,power-1);
+        }
+    }
+} 
